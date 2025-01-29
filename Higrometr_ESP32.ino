f8e7b13213b9a239ab3f80507dd728a8526d3e90
FishFingers001 <154709175+FishFingers001@users.noreply.github.com>
2025-01-29 17:57:25 +0100

    Update Higrometr_ESP32.ino

--- Higrometr_ESP32.ino
+++ Higrometr_ESP32.ino
@@ -6,16 +6,49 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+#define BUTTON_PIN 32
+#define DEBOUNCE_DELAY 50
+
+bool mode = false;  // false = Humidity Mode, true = Time Mode
+unsigned long lastDebounceTime = 0;
+bool lastButtonState = HIGH;
+
 LiquidCrystal lcd(13, 12, 27, 26, 25, 33);
 
 void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Use internal pull-up resistor
   lcd.begin(16, 2);
+  displayHumidity();  // Show initial message
   dht.begin();
   Serial.begin(9600);
 }
 
 void loop() {
-  float humidity = dht.readHumidity();
+  bool reading = digitalRead(BUTTON_PIN);
+
+  if (reading == LOW && lastButtonState == HIGH && millis() - lastDebounceTime > DEBOUNCE_DELAY) {
+        lastDebounceTime = millis();
+        mode = !mode;  // Toggle mode
+        updateDisplay();  // Update LCD
+    }
+    lastButtonState = reading;
+
+}
+void updateDisplay(){
+  lcd.clear();
+  if (mode) {
+    displayTime();
+  }
+  else {
+    displayHumidity();
+  }
+}
+void displayTime() {
+  lcd.setCursor(0, 0);
+  lcd.print("Czas");
+}
+void displayHumidity() {
+    float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
   if (isnan(humidity) || isnan(temperature)) {
@@ -35,3 +68,4 @@ void loop() {
 
   delay(2000);
 }
+
